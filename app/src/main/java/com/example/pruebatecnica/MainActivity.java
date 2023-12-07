57ded987e0dfb0bf8dc8a37c5f72ddb70349e705
Maxtro2002 <jspr200231@gmail.com>
2023-12-06 20:01:27 -0500

    Basic add logic

--- app/src/main/java/com/example/pruebatecnica/MainActivity.java
+++ app/src/main/java/com/example/pruebatecnica/MainActivity.java
@@ -3,14 +3,40 @@ package com.example.pruebatecnica;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-
+    EditText et1, et2;
+    TextView tv1;
+    Button btnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et1 = (EditText)findViewById(R.id.txtNumber1);
+        et2 = (EditText)findViewById(R.id.txtNumber2);
+        tv1 = (TextView)findViewById(R.id.lblResult);
+        btnAdd = (Button)findViewById(R.id.btnAdd);
+
+        btnAdd.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        float n1, n2, res;
+        n1 = Float.parseFloat(et1.getText().toString());
+        n2 = Float.parseFloat(et2.getText().toString());
+
+        res = n1 + n2;
+
+        tv1.setText("Resultado: " + res);
+
     }
 }
