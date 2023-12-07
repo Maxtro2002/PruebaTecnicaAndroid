558c6f36c10d760e26f409beebcb066dab70075e
Maxtro2002 <jspr200231@gmail.com>
2023-12-06 20:14:11 -0500

    Validations for code quality

--- app/src/main/java/com/example/pruebatecnica/MainActivity.java
+++ app/src/main/java/com/example/pruebatecnica/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.pruebatecnica;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -30,13 +31,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        float n1, n2, res;
-        n1 = Float.parseFloat(et1.getText().toString());
-        n2 = Float.parseFloat(et2.getText().toString());
+        try {
+            String input1 = et1.getText().toString();
+            String input2 = et2.getText().toString();
 
-        res = n1 + n2;
+            if (TextUtils.isEmpty(input1) || TextUtils.isEmpty(input2)) {
+                Toast.makeText(this, "Ingrese ambos números", Toast.LENGTH_SHORT).show();
+                return;
+            }
 
-        tv1.setText("Resultado: " + res);
+            float n1 = Float.parseFloat(input1);
+            float n2 = Float.parseFloat(input2);
 
+            float res = n1 + n2;
+            res = (float) (Math.round(res * 100.0) / 100.0);
+
+            tv1.setText("Resultado: " + res);
+        } catch (NumberFormatException e) {
+            Toast.makeText(this, "Ingrese números válidos", Toast.LENGTH_SHORT).show();
+        }
     }
+
 }
