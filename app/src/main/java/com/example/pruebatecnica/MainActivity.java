be5e1c66c4cd4247d29951d77a6889e0131cad6a
Maxtro2002 <jspr200231@gmail.com>
2023-12-06 23:56:00 -0500

    Variable name refactor

--- app/src/main/java/com/example/pruebatecnica/MainActivity.java
+++ app/src/main/java/com/example/pruebatecnica/MainActivity.java
@@ -12,8 +12,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    EditText et1, et2;
-    TextView tv1;
+    EditText editText1, editText2;
+    TextView textView;
     Button btnAdd;
 
     @Override
@@ -21,9 +21,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        et1 = (EditText)findViewById(R.id.txtNumber1);
-        et2 = (EditText)findViewById(R.id.txtNumber2);
-        tv1 = (TextView)findViewById(R.id.lblResult);
+        editText1 = (EditText)findViewById(R.id.txtNumber1);
+        editText2 = (EditText)findViewById(R.id.txtNumber2);
+        textView = (TextView)findViewById(R.id.lblResult);
         btnAdd = (Button)findViewById(R.id.btnAdd);
 
         btnAdd.setOnClickListener(this);
@@ -32,21 +32,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
         try {
-            String input1 = et1.getText().toString();
-            String input2 = et2.getText().toString();
+            String inputNumber1 = editText1.getText().toString();
+            String inputNumber2 = editText2.getText().toString();
 
-            if (TextUtils.isEmpty(input1) || TextUtils.isEmpty(input2)) {
+            if (TextUtils.isEmpty(inputNumber1) || TextUtils.isEmpty(inputNumber2)) {
                 Toast.makeText(this, "Ingrese ambos números", Toast.LENGTH_SHORT).show();
                 return;
             }
 
-            float n1 = Float.parseFloat(input1);
-            float n2 = Float.parseFloat(input2);
+            float number1 = Float.parseFloat(inputNumber1);
+            float number2 = Float.parseFloat(inputNumber2);
 
-            float res = n1 + n2;
+            float res = number1 + number2;
             res = (float) (Math.round(res * 100.0) / 100.0);
 
-            tv1.setText("Resultado: " + res);
+            textView.setText("Resultado: " + res);
         } catch (NumberFormatException e) {
             Toast.makeText(this, "Ingrese números válidos", Toast.LENGTH_SHORT).show();
         }
